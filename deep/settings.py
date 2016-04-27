5e8cbe21b35e6e0b46e3ffcac05e64dbc77b96dd
Frozen Helium <fren.ankit@gmail.com>
2016-04-27 20:10:46 +0545

    Add login and register page

--- deep/settings.py
+++ deep/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'deep.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
