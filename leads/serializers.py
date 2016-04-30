ccaffbf3b28875cb189084d833ea55790b64c544
bibek <bibekdahal.bd16@gmail.com>
2016-04-30 19:14:29 +0545

    Implement Lead form with Drag-Drop attachments

--- leads/serializers.py
+++ leads/serializers.py
@@ -7,9 +7,15 @@ from leads.models import *
 """ Lead serializer used by the REST api
 """
 class LeadSerializer(serializers.ModelSerializer):
+    attachments = serializers.SerializerMethodField()
     class Meta:
         model = Lead
         fields = ('id', 'name', 'source', 'content_format', 'assigned_to', 'published_at',
-                  'confidentiality', 'status', 'description', 'url', 'website', 'created_by')
+                  'confidentiality', 'status', 'description', 'url', 'website',
+                  'created_at', 'created_by', 'attachments')
 
         #TODO: Automatically set created_by.
+
+    def get_attachments(self, lead):
+        attachments = Attachment.objects.filter(lead=lead)
+        return [a.upload.url for a in attachments]
--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from django.forms import inlineformset_factory
+from datetime import datetime
 
 from leads.models import *
 
@@ -38,4 +39,42 @@ class AddManual(View):
 
     @method_decorator(login_required)
     def post(self, request):
-        return redirect("leads:add-manual")
+
+        error = ""
+
+        # Create new lead with POST data.
+        lead = Lead()
+        lead.name = request.POST["name"]
+
+        if request.POST["source"] != "":
+            lead.source = Source.objects.get(pk=request.POST["source"])
+
+        if request.POST["content-format"] != "":
+            lead.content_format = request.POST["content-format"]
+
+        lead.confidentiality = request.POST["confidentiality"]
+
+        if request.POST["assigned-to"] != "":
+            lead.assigned_to = User.objects.get(pk=request.POST["assigned-to"])
+
+        if request.POST["publish-date"] != "":
+            lead.published_at = datetime.strptime(request.POST["publish-date"], "%d-%m-%Y").date()
+
+        lead.created_by = request.user
+        lead.description = request.POST["description"]
+        lead.save()
+
+        for file in request.FILES:
+            attachment = Attachment()
+            attachment.lead = lead
+            attachment.upload = request.FILES[file]
+            attachment.save()
+
+        if error != "":
+            return render(request, "leads/add-manual.html",
+                          {"error": error})
+
+        # if request.POST["post"] == "save-and-another":
+        #     return redirect("leads:add-manual")
+        # else:
+        return redirect("leads:leads")
