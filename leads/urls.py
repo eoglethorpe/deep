5a9af32b842dad935626d173ecb5b67bbcb9ad33
bibek <bibekdahal.bd16@gmail.com>
2016-05-01 12:09:27 +0545

    Implement lead editing

--- leads/urls.py
+++ leads/urls.py
@@ -5,4 +5,5 @@ from leads import views
 urlpatterns = [
     url(r'^$', views.LeadsView.as_view(), name="leads"),
     url(r'^add-manual/$', views.AddManual.as_view(), name='add-manual'),
+    url(r'^add-manual/(?P<id>\d+)/$', views.AddManual.as_view(), name='add-manual'),
 ]
