02f94a3cea9ebfdfb844ce1809147ddcdb8fa130
eoglethorpe <ewanogle@gmail.com>
2016-07-27 15:46:08 +0545

    initial commit setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='DEEP',
+    version='0.1',
+    install_requires=['django','readability-lxml', 'requests', \
+                'pdfminer3k', 'openpyxl', 'docx'],
+)
