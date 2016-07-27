28a5a09753f0d9f094dc24f79c5311499b3f986f
eoglethorpe <ewanogle@gmail.com>
2016-07-27 17:08:32 +0545

    add all needed dependacies to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='DEEP',
     version='0.1',
-    install_requires=['django','readability-lxml', 'requests', \
-                'pdfminer3k', 'openpyxl', 'docx'],
-)
+    install_requires=['django', 'djangorestframework', 'mysqlclient', 'readability-lxml', 'requests', \
+                'pdfminer3k', 'openpyxl', 'python-docx', 'pillow', 'python-dateutil', 'beautifulsoup4'],
+)   . 
