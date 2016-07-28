17b3d26feda1581b571d0d4db532d4186c19e70e
Frozen Helium <fren.ankit@gmail.com>
2016-07-28 09:30:21 +0545

    Remove a sneaky dot from setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(
     version='0.1',
     install_requires=['django', 'djangorestframework', 'mysqlclient', 'readability-lxml', 'requests', \
                 'pdfminer3k', 'openpyxl', 'python-docx', 'pillow', 'python-dateutil', 'beautifulsoup4'],
-)   . 
+) 
