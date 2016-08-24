f8fc7fc5b6e472f0ae3d665585788704ea5d16e7
bibek <bibekdahal.bd16@gmail.com>
2016-08-24 22:25:57 +0545

    Add report dashboard navigation link

--- report/urls.py
+++ report/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from report import views
 
 urlpatterns = [
-#    url(r'^$', views.DashboardView.as_view(), name="dashboard"),
+    url(r'^$', views.ReportDashboardView.as_view(), name="dashboard"),
     url(r'^weekly/$', views.WeeklyReportView.as_view(), name="weekly"),
     url(r'^monthly/$', views.MonthlyReportView.as_view(), name="monthly"),
 ]
--- report/views.py
+++ report/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View
 
-# class DashboardView(View):
-#     def get(self, request):
-#         return render(request, "report/dashboard.html")
+class ReportDashboardView(View):
+    def get(self, request):
+        return render(request, "report/dashboard.html")
 
 class WeeklyReportView(View):
     def get(self, request):
