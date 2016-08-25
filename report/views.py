478c16e29d5c2e0cc89d32e8b5ef588989cf32e5
Frozen Helium <fren.ankit@gmail.com>
2016-08-25 13:27:42 +0545

    Add countries to report dash

--- report/views.py
+++ report/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from leads.models import *
+from entries.models import *
+
+
 class ReportDashboardView(View):
     def get(self, request):
-        return render(request, "report/dashboard.html")
+        context = {}
+        context["countries"] = Country.objects.all()
+        return render(request, "report/dashboard.html", context)
 
 class WeeklyReportView(View):
     def get(self, request):
