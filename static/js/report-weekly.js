7d6414a3ae83497c72971964d40118025a1ee4e9
Aditya Khatri <adityakhatri47@gmail.com>
2016-09-30 16:50:11 +0545

    Add fields in weekly report page

--- static/js/report-weekly.js
+++ static/js/report-weekly.js
@@ -10,4 +10,22 @@ $(document).ready(function(){
     });
 
     $('.filter').selectize();
+    $('#disaster-type-select').selectize();
+    $('#status-select').selectize();
+
+    function addEventTimeline(){
+        var container = $('#event-timeline-container');
+        var event_timeline = $('.event-timeline-template').clone();
+
+        event_timeline.removeClass('event-timeline-template');
+        event_timeline.addClass('event-timeline');
+        event_timeline.find('select').selectize();
+        event_timeline.appendTo(container);
+        event_timeline.show();
+
+        event_timeline.find('button').on('click', addEventTimeline);
+
+    }
+
+    addEventTimeline();
 });
