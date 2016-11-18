f7835ea45838b1ac7c96f38a33a5bc92c031a942
Aditya Khatri <adityakhatri47@gmail.com>
2016-11-18 14:49:14 +0545

    Redesign dashboard view

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,2 +1,18 @@
-$(document).ready(function() {
-} );
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+$(document).ready(function(){
+    var map = L.map('the-map').setView([41.87, 12.6], 2);
+
+    $.getJSON('/static/files/countries.geo.json', function(data) {
+        var layer = L.geoJson(data, {style: style}).addTo(map);
+    });
+});
