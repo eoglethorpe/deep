0791268364ecd62cc4a46f819bd2ee08b3554626
Aditya Khatri <adityakhatri47@gmail.com>
2016-11-23 13:21:59 +0545

    Add elements to visualizations page

--- static/js/entry-visualization.js
+++ static/js/entry-visualization.js
@@ -1,73 +1,93 @@
 $(document).ready(function(){
     google.charts.load('current', {packages: ['corechart', 'bar']});
-    google.charts.setOnLoadCallback(drawAxisTickColors);
-    console.log('FUN');
+    google.charts.setOnLoadCallback(sectorBar);
 
-    function drawAxisTickColors() {
-        var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population', '2000 Population'],
-        ['New York City, NY', 8175000, 8008000],
-        ['Los Angeles, CA', 3792000, 3694000],
-        ['Chicago, IL', 2695000, 2896000],
-        ['Houston, TX', 2099000, 1953000],
-        ['Philadelphia, PA', 1526000, 1517000]
-        ]);
+    function sectorBar() {
+            var data = google.visualization.arrayToDataTable([
+                ['Genre', 'No problem', 'Minor problem', 'Situation of concern', 'Situation of major concern',
+                'Severe conditions', 'Critical situation', { role: 'annotation' } ],
+                ['2020', 16, 22, 23, 30, 16, 9, ''],
+                ['2030', 28, 19, 29, 30, 12, 13, '']
+    ]);
 
         var options = {
-            title: 'Population of Largest U.S. Cities',
-            chartArea: {width: '50%'},
-            backgroundColor: '#fafafa',
-            hAxis: {
-                title: 'Total Population',
-                minValue: 0,
-                textStyle: {
-                    bold: true,
-                    fontSize: 12,
-                    color: '#4d4d4d'
-              },
-              titleTextStyle: {
-                  bold: true,
-                  fontSize: 18,
-                  color: '#4d4d4d'
-              }
-            },
-            vAxis: {
-                title: 'City',
-                textStyle: {
-                    fontSize: 14,
-                    bold: true,
-                    color: '#848484'
-                },
-              titleTextStyle: {
-                  fontSize: 14,
-                  bold: true,
-                  color: '#848484'
-              }
-          }
+            width: 400,
+            height: 1000,
+            backgroundColor: 'transparent',
+            legend: { position: 'top', maxLines: 3 },
+            bar: { groupWidth: '10%' },
+            isStacked: true,
+            series: {
+                0:{color:'#f2adad'},
+                1:{color:'#ec8080'},
+                2:{color:'#e55454'},
+                3:{color:'#cd1f1f'},
+                4:{color:'#cd1f1f'},
+                5:{color:'#b71c1c'}
+            }
         };
-        var chart = new google.visualization.BarChart(document.getElementById('chart-div'));
+        var chart = new google.visualization.BarChart(document.getElementById('sector-bar'));
         chart.draw(data, options);
     }
 
-    google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
         ]);
 
         var options = {
-            backgroundColor: '#fafafa',
+            width: 400,
+            height: 400,
+            backgroundColor: 'transparent',
             title: 'My Daily Activities',
+            legend: { position: 'top', maxLines: 3 },
             pieHole: 0.4,
+            slices: {
+                0:{color:'#f2adad'},
+                1:{color:'#ec8080'},
+                2:{color:'#e55454'},
+                3:{color:'#cd1f1f'},
+                4:{color:'#cd1f1f'},
+                5:{color:'#b71c1c'}
+            }
         };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        var chart = new google.visualization.PieChart(document.getElementById('donut-chart'));
         chart.draw(data, options);
-      }
+    }
+
+    google.charts.setOnLoadCallback(timeBar);
+    function timeBar() {
+        var data = google.visualization.arrayToDataTable([
+            ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
+            'Western', 'Literature', { role: 'annotation' } ],
+            ['2010', 10, 24, 20, 32, 18, 5, ''],
+            ['2020', 16, 22, 23, 30, 16, 9, ''],
+            ['2030', 28, 19, 29, 30, 12, 13, '']
+        ]);
+
+        var options = {
+            width: 1000,
+            height: 400,
+            backgroundColor: 'transparent',
+            legend: { position: 'top', maxLines: 3 },
+            bar: { groupWidth: '2%' },
+            isStacked: true,
+            series: {
+                0:{color:'#f2adad'},
+                1:{color:'#ec8080'},
+                2:{color:'#e55454'},
+                3:{color:'#cd1f1f'},
+                4:{color:'#cd1f1f'},
+                5:{color:'#b71c1c'}
+            }
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("time-bar"));
+        chart.draw(data, options);
+    }
 });
