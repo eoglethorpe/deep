a3a3e7cc3bdbc895bacbe2ba3f9b41744a365a6d
Bibek Dahal <bibekdahal.bd16@gmail.com>
2016-12-29 14:14:11 +0545

    Add date, period parameters to weekly report data

--- static/js/common/utils.js
+++ static/js/common/utils.js
@@ -123,3 +123,69 @@ function getContrastYIQ(hexcolor){
     var yiq = ((r*299)+(g*587)+(b*114))/1000;
     return (yiq >= 128) ? '#212121' : '#fff';
 }
+
+
+// http://techblog.procurios.nl/k/news/view/33796/14863/calculate-iso-8601-week-and-year-in-javascript.html
+/**
+ * Get the ISO week date week number
+ */
+Date.prototype.getWeek = function () {
+    // Create a copy of this date object
+    var target  = new Date(this.valueOf());
+
+    // ISO week date weeks start on monday
+    // so correct the day number
+    var dayNr   = (this.getDay() + 6) % 7;
+
+    // ISO 8601 states that week 1 is the week
+    // with the first thursday of that year.
+    // Set the target date to the thursday in the target week
+    target.setDate(target.getDate() - dayNr + 3);
+
+    // Store the millisecond value of the target date
+    var firstThursday = target.valueOf();
+
+    // Set the target to the first thursday of the year
+    // First set the target to january first
+    target.setMonth(0, 1);
+    // Not a thursday? Correct the date to the next thursday
+    if (target.getDay() != 4) {
+        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+    }
+
+    // The weeknumber is the number of weeks between the
+    // first thursday of the year and the thursday in the target week
+    return 1 + Math.ceil((firstThursday - target) / 604800000); // 604800000 = 7 * 24 * 3600 * 1000
+}
+
+/**
+* Get the ISO week date year number
+*/
+Date.prototype.getWeekYear = function ()
+{
+    // Create a new date object for the thursday of this week
+    var target  = new Date(this.valueOf());
+    target.setDate(target.getDate() - ((this.getDay() + 6) % 7) + 3);
+
+    return target.getFullYear();
+}
+
+// http://stackoverflow.com/a/16591175/4328459
+function getDateOfISOWeek(w, y) {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var dow = simple.getDay();
+    var ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+    return ISOweekStart;
+}
+
+function getStupidDateFormat(date) {
+    // dd-mmm-yyyy
+    var dd = date.toLocaleString('en-us', {day: '2-digit'});
+    var mmm = date.toLocaleString('en-us', {month: 'short'});
+    var yyyy = date.toLocaleString('en-us', {year: 'numeric'});
+    return dd + '-' + mmm + '-' + yyyy;
+}
