0752ed851f362999f58e9a39f066e731fb485bcb
Ubuntu <ubuntu@ip-172-31-60-141.ec2.internal>
2017-03-07 22:59:21 +0000

    add email settings

--- deep/settings.py
+++ deep/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#Email
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER = 'deepnotifications1@gmail.com'
+EMAIL_HOST_PASSWORD = 'deep1234'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+ADMINS = [()]
 
 # Application definition
 
