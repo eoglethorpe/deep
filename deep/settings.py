0ac759a5e502c7c59dcead1f0adbf0c0f21d237b
Navin IR <ayernavin@gmail.com>
2017-03-15 15:15:30 +0545

    Use Docker Volumes for static and media files

--- deep/settings.py
+++ deep/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
-    'django_cleanup', 
+    'django_cleanup',
     'users',
     'leads',
     'entries',
@@ -102,16 +102,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 
@@ -142,9 +146,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+if os.environ.get('IN_DOCKER'):
+    STATIC_ROOT = os.environ['STATIC_ROOT']
+    MEDIA_ROOT = os.environ['MEDIA_ROOT']
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # django-cors-headers configurations
 CORS_ORIGIN_ALLOW_ALL = True
