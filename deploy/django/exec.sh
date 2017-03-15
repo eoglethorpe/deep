aa5d5dbabcf6edd8e119ab04750d5a2cbe1ba2b3
Navin IR <ayernavin@gmail.com>
2017-03-15 16:02:42 +0545

    Add adminload in exec.sh
    Add Build args in docker-compose for cache miss for new build

--- deploy/django/exec.sh
+++ deploy/django/exec.sh
@@ -4,8 +4,8 @@
 python /home/code/deep/manage.py migrate --noinput
 python /home/code/deep/manage.py collectstatic --noinput
 
-#python /home/code/deep/manage.py load_admin0
-#python /home/code/deep/manage.py load_admin1
-#python /home/code/deep/manage.py load_admin2
+python /home/code/deep/manage.py load_admin0
+python /home/code/deep/manage.py load_admin1
+python /home/code/deep/manage.py load_admin2
 
 /home/code/venv/bin/uwsgi --ini /home/code/deep/deploy/django/uwsgi.ini
