5b84ca7f69c293490827b5a624b9d16c7404ff76
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-03-23 21:21:20 +0545

    Add matrix 1d widget

--- static/js/entry-template/entry-template.js
+++ static/js/entry-template/entry-template.js
@@ -8,16 +8,86 @@ class Element {
     constructor(container, dom) {
         this.dom = dom;
         container.append(dom);
-        dom.draggable({ grid: [16, 16], containment: container, });
+        if (dom.find('.handle').length > 0) {
+            dom.draggable({ grid: [16, 16], containment: container, handle: '.handle' });
+        } else {
+            dom.draggable({ grid: [16, 16], containment: container });
+        }
     }
 };
 
 class Matrix1D extends Element {
+    constructor(container) {
+        let dom = $('<div class="element matrix1d"></div>');
+        dom.append($('<div class="fa fa-arrows handle"></div>'));
+        dom.append($('<div class="pillars sortable"></div>'));
+        dom.append($('<button class="fa fa-plus add-pillar"></button>'));
+        super(container, dom);
+        let that = this;
+
+        this.addPillar();
+        dom.find('.add-pillar').click(function() {
+            that.addPillar();
+        });
+
+        this.dom.find('.pillars').sortable({ axis: 'y' });
+    }
+
+    addPillar() {
+        let that = this;
+
+        let pillar = $('<div class="pillar"></div>');
+        pillar.append($('<div class="title-block">New pillar</div>'));
+        pillar.append($('<div class="subpillars sortable"></div>'));
+        pillar.append($('<button class="fa fa-plus add-subpillar"></button>'));
+        pillar.prepend($('<button class="fa fa-times remove-pillar"></button>'));
+        this.dom.find('.pillars').append(pillar);
+
+        this.addSubpillar(pillar);
+        pillar.find('.add-subpillar').click(function() {
+            that.addSubpillar(pillar);
+        });
+        pillar.find('.remove-pillar').click(function() {
+            pillar.remove();
+        });
+
+        this.makeEditable(pillar.find('.title-block'));
+        pillar.find('.subpillars').sortable({ axis: 'x' });
+    }
+
+    addSubpillar(pillar) {
+        let subpillar = $('<div class="subpillar" tabIndex="1"></div>');
+        subpillar.append($('<div class="title-block">New subpillar</div>'));
+        subpillar.append($('<button class="fa fa-times remove-subpillar"></button>'))
+        pillar.find('.subpillars').append(subpillar);
+
+        subpillar.find('.remove-subpillar').click(function() {
+            subpillar.remove();
+        });
+
+        this.makeEditable(subpillar.find('.title-block'));
+    }
+
+    makeEditable(element) {
+        element.click(function() {
+            $(this).closest('.element').find('div').attr('contenteditable', 'false');
+            $(this).attr('contenteditable', 'true');
+            $(this).closest('.element').draggable({ disabled: true });
+            $(this).parents('.sortable').sortable({ disabled: true });
+            $(this).focus();
+        });
+
+        element.blur(function(e) {
+            $(this).attr('contenteditable', 'false');
+            $(this).closest('.element').draggable({ disabled: false });
+            $(this).parents('.sortable').sortable({ disabled: false });
+        });
+    }
 };
 
 class NoobWidget extends Element {
     constructor(container) {
-        let dom = $('<div class="widget noob-widget">Noob</div>');
+        let dom = $('<div class="element noob-widget">Noob</div>');
         super(container, dom);
     }
 };
@@ -28,6 +98,10 @@ let templateEditor = {
         let that = this;
         $('#noob-widget button').on('click', function() {
             that.addElement(new NoobWidget($('main')));
+        });
+
+        $('#matrix1d-widget button').on('click', function() {
+            that.addElement(new Matrix1D($('main')));
         });
     },
 
