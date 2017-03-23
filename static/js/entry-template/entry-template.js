5e4e8ea23c36c5301e1184ef941345222ccdd6cd
Frozen Helium <fren.ankit@gmail.com>
2017-03-23 15:31:36 +0545

    Add basic entry template editor

--- static/js/entry-template/entry-template.js
+++ static/js/entry-template/entry-template.js
@@ -1,3 +1,43 @@
-$(document).ready(function() {
+let template = {
+    name: 'Untitled',
+    elements: [],
+};
 
+
+class Element {
+    constructor(container, dom) {
+        this.dom = dom;
+        container.append(dom);
+        dom.draggable({ grid: [16, 16], containment: container, });
+    }
+};
+
+class Matrix1D extends Element {
+};
+
+class NoobWidget extends Element {
+    constructor(container) {
+        let dom = $('<div class="widget noob-widget">Noob</div>');
+        super(container, dom);
+    }
+};
+
+
+let templateEditor = {
+    init: function() {
+        let that = this;
+        $('#noob-widget button').on('click', function() {
+            that.addElement(new NoobWidget($('main')));
+        });
+    },
+
+    addElement: function(element) {
+        template.elements.push(element);
+    },
+};
+
+
+$(document).ready(function() {
+    templateEditor.init();
+    $('#elements').sortable();
 });
