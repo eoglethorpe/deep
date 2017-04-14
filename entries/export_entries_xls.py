192a10d8ff9e0606e13f2a78d816bd07650f2821
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-04-14 10:30:16 +0545

    Fix #377

--- entries/export_entries_xls.py
+++ entries/export_entries_xls.py
@@ -5,6 +5,13 @@ from entries import models as entry_models
 from openpyxl.styles import Font  # , Color
 
 
+def format_date(date):
+    if date:
+        return date.strftime('%d-%m-%Y')
+    else:
+        return None
+
+
 def export_xls(title, event_pk=None):
 
     # Create a spreadsheet and get active workbook
@@ -19,7 +26,6 @@ def export_xls(title, event_pk=None):
         "Date Imported", "Lead Title", "Source", "Excerpt", "Reliability",
         "Severity", "Number", "Demographic Groups", "Specific Needs Groups",
         "Affected Groups", "Pillar", "Subpillar", "Sector", "Subsector",
-        "Date Imported"
     ]
 
     if event_pk:
@@ -52,48 +58,54 @@ def export_xls(title, event_pk=None):
 
     grouped_rows = []
     for i, info in enumerate(informations):
-        rows = RowCollection(1)
+        try:
+            rows = RowCollection(1)
 
-        rows.add_values([
-            info.entry.lead.published_at, info.date, info.entry.modified_by,
-            info.entry.modified_at.date(), info.entry.lead.name,
-            info.entry.lead.source, xstr(info.excerpt),
-            info.reliability.name, info.severity.name, info.number
-        ])
+            rows.add_values([
+                format_date(info.entry.lead.published_at), format_date(info.date), info.entry.created_by,
+                format_date(info.entry.created_at.date()), info.entry.lead.name,
+                info.entry.lead.source_name, xstr(info.excerpt),
+                info.reliability.name, info.severity.name, info.number
+            ])
 
-        # Column Name `Demographic Groups` Renamed to
-        # `Vulnerable Group` as specified in Issue #280
-        rows.permute_and_add(info.vulnerable_groups.all())
-        rows.permute_and_add(info.specific_needs_groups.all())
-        rows.permute_and_add(info.affected_groups.all())
+            # Column Name `Demographic Groups` Renamed to
+            # `Vulnerable Group` as specified in Issue #280
+            rows.permute_and_add(info.vulnerable_groups.all())
+            rows.permute_and_add(info.specific_needs_groups.all())
+            rows.permute_and_add(info.affected_groups.all())
 
-        attributes = []
-        for attr in info.informationattribute_set.all():
-            attr_data = [attr.subpillar.pillar.name, attr.subpillar.name]
+            attributes = []
+            if info.informationattribute_set.count() > 0:
+                for attr in info.informationattribute_set.all():
+                    attr_data = [attr.subpillar.pillar.name, attr.subpillar.name]
 
-            if attr.sector:
-                attr_data.append(attr.sector.name)
-                if attr.subsectors.count() > 0:
-                    for ss in attr.subsectors.all():
-                        attributes.append(attr_data + [ss.name])
-                else:
-                    attributes.append(attr_data + [''])
+                    if attr.sector:
+                        attr_data.append(attr.sector.name)
+                        if attr.subsectors.count() > 0:
+                            for ss in attr.subsectors.all():
+                                attributes.append(attr_data + [ss.name])
+                        else:
+                            attributes.append(attr_data + [''])
+                    else:
+                        attributes.append(attr_data + ['', ''])
             else:
-                attributes.append(attr_data + ['', ''])
+                attributes.append(['', '', '', ''])
 
-        rows.permute_and_add_list(attributes)
+            rows.permute_and_add_list(attributes)
 
-        for country in countries:
-            admin_levels = country.adminlevel_set.all()
-            for admin_level in admin_levels:
-                selections = []
-                for map_selection in info.map_selections.all():
-                    if admin_level == map_selection.admin_level:
-                        selections.append(map_selection.name)
-                rows.permute_and_add(selections)
+            for country in countries:
+                admin_levels = country.adminlevel_set.all()
+                for admin_level in admin_levels:
+                    selections = []
+                    for map_selection in info.map_selections.all():
+                        if admin_level == map_selection.admin_level:
+                            selections.append(map_selection.name)
+                    rows.permute_and_add(selections)
 
-        ew.append(rows.rows, ws)
-        grouped_rows.append(rows.group_rows)
+            ew.append(rows.rows, ws)
+            grouped_rows.append(rows.group_rows)
+        except:
+            pass
 
     ew.append(grouped_rows, wsg)
 
@@ -109,18 +121,18 @@ def export_and_save(event_pk, filename):
 
     # Create title row
     titles = [
-        "Date of Lead Publication", "Date of Information", "Created By",
+        "Country", "Date of Lead Publication", "Date of Information", "Created By",
         "Date Imported", "Lead Title", "Source", "Excerpt", "Reliability",
         "Severity", "Number", "Demographic Groups", "Specific Needs Groups",
         "Affected Groups", "Pillar", "Subpillar", "Sector", "Subsector",
-        "Date Imported",
     ]
 
     countries = entry_models.Event.objects.get(pk=event_pk).countries.all().distinct()
+
     for country in countries:
         admin_levels = country.adminlevel_set.all()
-        for admin_level in admin_levels:
-            titles.append(admin_level.name)
+        for i, admin_level in enumerate(admin_levels):
+            titles.append('Admin {}'.format(i))
 
     for i, t in enumerate(titles):
         ws.cell(row=1, column=i+1).value = t
@@ -135,15 +147,18 @@ def export_and_save(event_pk, filename):
     # Add each information in each entry belonging to this event
     informations = entry_models.EntryInformation.objects.filter(
                         entry__lead__event__pk=event_pk, entry__template=None).distinct()
+
     grouped_rows = []
     for i, info in enumerate(informations):
         try:
             rows = RowCollection(1)
 
+            rows.permute_and_add(info.entry.lead.event.countries.all())
+
             rows.add_values([
-                info.entry.lead.published_at, info.date, info.entry.modified_by,
-                info.entry.modified_at.date(), info.entry.lead.name,
-                info.entry.lead.source, xstr(info.excerpt),
+                format_date(info.entry.lead.published_at), format_date(info.date), info.entry.created_by,
+                format_date(info.entry.created_at.date()), info.entry.lead.name,
+                info.entry.lead.source_name, xstr(info.excerpt),
                 info.reliability.name, info.severity.name, info.number
             ])
 
@@ -154,18 +169,21 @@ def export_and_save(event_pk, filename):
             rows.permute_and_add(info.affected_groups.all())
 
             attributes = []
-            for attr in info.informationattribute_set.all():
-                attr_data = [attr.subpillar.pillar.name, attr.subpillar.name]
+            if info.informationattribute_set.count() > 0:
+                for attr in info.informationattribute_set.all():
+                    attr_data = [attr.subpillar.pillar.name, attr.subpillar.name]
 
-                if attr.sector:
-                    attr_data.append(attr.sector.name)
-                    if attr.subsectors.count() > 0:
-                        for ss in attr.subsectors.all():
-                            attributes.append(attr_data + [ss.name])
+                    if attr.sector:
+                        attr_data.append(attr.sector.name)
+                        if attr.subsectors.count() > 0:
+                            for ss in attr.subsectors.all():
+                                attributes.append(attr_data + [ss.name])
+                        else:
+                            attributes.append(attr_data + [''])
                     else:
-                        attributes.append(attr_data + [''])
-                else:
-                    attributes.append(attr_data + ['', ''])
+                        attributes.append(attr_data + ['', ''])
+            else:
+                attributes.append(['', '', '', ''])
 
             rows.permute_and_add_list(attributes)
 
@@ -180,8 +198,8 @@ def export_and_save(event_pk, filename):
 
             ew.append(rows.rows, ws)
             grouped_rows.append(rows.group_rows)
-        except Exception as error:
-            print(error)
+        except:
+            pass
 
     ew.append(grouped_rows, wsg)
     ew.save_to(filename)
