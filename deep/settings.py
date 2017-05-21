f64e8e58d13146bc452abed6b9c1892f248951f0
thenav56 <ayernavin@gmail.com>
2017-05-21 11:22:54 +0545

    GET DEBUG FROM ENV

--- deep/settings.py
+++ deep/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2egrwk!cwh6y$6lzpvmc6+lsgp417@)g0c=u^cguz(n9#-!p75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 
 def get_pub_ip():
