687d764b2030764ca3916461671f9d387a1e9421
thenav56 <ayernavin@gmail.com>
2017-07-27 13:27:51 +0545

    No Auth url for static files

--- custom_admin/templatetags/watchful_url.py
+++ custom_admin/templatetags/watchful_url.py
@@ -11,11 +11,6 @@ register = template.Library()
 
 @register.simple_tag
 def watchful_static_url(path):
-    if getattr(settings, 'AWS_QUERYSTRING_AUTH', False):
-        # TODO: Remove later, use AWS cache timer
-        return static(path) + '&modified_date_time=' +\
-                str(os.path.getmtime(os.path.join(
-                    os.path.join(settings.BASE_DIR, 'static'), path)))
     return static(path) + '?modified_date_time=' +\
         str(os.path.getmtime(os.path.join(
                 os.path.join(settings.BASE_DIR, 'static'), path)))
