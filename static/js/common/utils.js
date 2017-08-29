7c9af11a7691f9386ae962fce746a49a695aa04c
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-08-29 17:34:38 +0545

    Put thousand separators

--- static/js/common/utils.js
+++ static/js/common/utils.js
@@ -340,6 +340,24 @@ function reformatText(text) {
     return text.trim();
 }
 
+function putThousandSeparators(value, sep) {
+    if (sep == null) {
+        sep = ',';
+    }
+    // check if it needs formatting
+    if (value.toString() === value.toLocaleString()) {
+        // split decimals
+        var parts = value.toString().split('.')
+        // format whole numbers
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, sep);
+        // put them back together
+        value = parts[1] ? parts.join('.') : parts[0];
+    } else {
+        value = value.toLocaleString();
+    }
+    return value;
+}
+
 // Migrate object keys based on given model
 function migrate(data, dataModel) {
     // TODO:
