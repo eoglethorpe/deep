f025a9c9fdc8e79eeb6c9c2ba6245b28a52e712b
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-07-20 12:37:16 +0545

    Fix issues in original preview

--- static/js/entries/add-template-entry/page-one.js
+++ static/js/entries/add-template-entry/page-one.js
@@ -3,6 +3,7 @@ let page1 = {
         let that = this;
         this.selectedEntryIndex = -1;
         this.container = $('#page-one-elements');
+        this.validateOriginalView();
 
         for (let i=0; i<templateData.elements.length; i++) {
             let element = templateData.elements[i];
@@ -435,4 +436,64 @@ let page1 = {
 
         addOrReplaceEntry(excerpt, image);
     },
+
+    validateOriginalView: function() {
+        let response = {};
+        $.post(websiteInfoUrl, {'url': leadUrl})
+            .done((r) => {
+                response = r;
+            })
+            .fail(() => {
+                response = {'errorCode': 3, 'error': 'Failure'};
+            })
+            .always(() => {
+                let canShowIframe = true;
+                if (response.error) {
+                    canShowIframe = false;
+                } else {
+                    const { headers } = response;
+                    const xFrameOptions = getHeaderValue(headers, 'X-Frame-Options');
+                    const contentSecurityPolicy = getHeaderValue(headers, 'Content-Security-Policy');
+                    const contentType = getHeaderValue(headers, 'Content-Type');
+
+                    if (leadUrl.startsWith(location.origin)) {
+                        return;
+                    }
+                    if (contentType === 'application/pdf') {
+                        $('#lead-preview').replaceWith("<iframe id='lead-preview' src='" + leadUrl + "'></iframe>");
+                        leadPreviewer.changePreview($('input[type=radio][name=lead-view-option]').val());
+                    }
+                    // TODO Also check for doc, ppt and excel content types
+
+                    // Older policy
+                    if (xFrameOptions) {
+                        const options = xFrameOptions.toLowerCase();
+                        if (options.match('sameorigin|deny|allow-from')) {
+                            canShowIframe = false;
+                        }
+                    }
+
+                    // New policy
+                    if (canShowIframe && contentSecurityPolicy) {
+                        const options = contentSecurityPolicy.toLowerCase();
+                        if (options.match('frame-ancestors')) {
+                            canShowIframe = false;
+                        }
+                    }
+                }
+
+                if (!canShowIframe) {
+                    $('#lead-preview').replaceWith("<div id='lead-preview'><div class='error'><div>We cannot currently preview this publisher. Please open it in a new tab or copy the URL.</div><a href='" + leadUrl + "' target='_blank'>" + leadUrl + "</a></div></div>");
+                    leadPreviewer.changePreview($('input[type=radio][name=lead-view-option]').val());
+                }
+            });
+    },
 };
+
+
+function getHeaderValue(headers, header) {
+    const tHeader = Object.keys(headers).find(
+        key => key.toLowerCase() === header.toLowerCase(),
+    );
+    return headers[tHeader];
+}
